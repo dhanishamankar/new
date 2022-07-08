a5f8f503ce6dbe13bc7767b9f6313cc796c35f83
dhanishamankar <dhanishamankar22@gmail.com>
2022-07-08 15:31:56 +0530

    first commit

---
+++ src/components/Products.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import { popularProducts } from "../data";
+import Product from "./Product";
+
+const Container = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`;
+
+const Products = () => {
+  return (
+    <Container>
+      {popularProducts.map((item) => (
+        <Product item={item} key={item.id} />  //key should be there if map used,unique item is prop id is in data popular product
+      ))}
+    </Container>
+  );
+};
+
+export default Products;
